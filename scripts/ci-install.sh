e37c376c04b20bb414a8bb4e344a72d2d1c339d3
Jookia <contact@jookia.org>
2023-03-13 00:05:02 +1100

    klippy: Drop support for python2
    It's infeasible at this point to ask people to test their code on
    Python 2, so just drop support for it.

--- scripts/ci-install.sh
+++ scripts/ci-install.sh
@@ -16,7 +16,7 @@ mkdir -p ${BUILD_DIR} ${CACHE_DIR}
 ######################################################################
 
 echo -e "\n\n=============== Install system dependencies\n\n"
-PKGS="virtualenv python2-dev libffi-dev build-essential"
+PKGS="virtualenv libffi-dev build-essential"
 PKGS="${PKGS} gcc-avr avr-libc"
 PKGS="${PKGS} libnewlib-arm-none-eabi gcc-arm-none-eabi binutils-arm-none-eabi"
 PKGS="${PKGS} pv libmpfr-dev libgmp-dev libmpc-dev texinfo bison flex"
@@ -70,13 +70,3 @@ echo -e "\n\n=============== Install python3 virtualenv\n\n"
 cd ${MAIN_DIR}
 virtualenv -p python3 ${BUILD_DIR}/python-env
 ${BUILD_DIR}/python-env/bin/pip install -r ${MAIN_DIR}/scripts/klippy-requirements.txt
-
-
-######################################################################
-# Create python2 virtualenv environment
-######################################################################
-
-echo -e "\n\n=============== Install python2 virtualenv\n\n"
-cd ${MAIN_DIR}
-virtualenv -p python2 ${BUILD_DIR}/python2-env
-${BUILD_DIR}/python2-env/bin/pip install -r ${MAIN_DIR}/scripts/klippy-requirements.txt
