695b86c755cae5155a0bfc332a3e40f20c77eb1c
Rogerio Goncalves <rogerlz@gmail.com>
2025-03-25 14:47:34 +0000

    stm32g4/h7 - fix pll_base when using a 25mhz crystal (#623)

--- src/stm32/stm32h7.c
+++ src/stm32/stm32h7.c
@@ -98,18 +98,17 @@ clock_setup(void)
     PWR->CR3 = (PWR->CR3 | PWR_CR3_LDOEN) & ~(PWR_CR3_BYPASS | PWR_CR3_SCUEN);
     while (!(PWR->CSR1 & PWR_CSR1_ACTVOSRDY))
         ;
-// klipper supports 8, 12, 16, 20, 24, and 25 MHz crystals on HSE
-#if CONFIG_CLOCK_REF_FREQ % 5000000 == 0
-    uint32_t pll_base = 5000000;
-#elif CONFIG_CLOCK_REF_FREQ % 4000000 == 0
-    uint32_t pll_base = 4000000;
-#else
-#error Unknown pll_base for CLOCK_REF_FREQ
-#endif
     // Only even dividers (DIVP1) are allowed
     uint32_t pll_freq = CONFIG_CLOCK_FREQ * 2;
+    uint32_t pll_base;
+
     if (!CONFIG_STM32_CLOCK_REF_INTERNAL) {
         // Configure PLL from external crystal (HSE)
+#if CONFIG_CLOCK_REF_FREQ % 5000000 == 0
+        pll_base = 5000000;
+#else
+        pll_base = 4000000;
+#endif
         RCC->CR |= RCC_CR_HSEON;
         while(!(RCC->CR & RCC_CR_HSERDY))
             ;
